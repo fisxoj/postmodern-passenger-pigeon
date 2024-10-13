fad01e6b7108ebf2d12f2ac4c39c60d0518cc7db
Matt Novenstern <fisxoj@gmail.com>
2024-10-13 19:58:06 -0400

    pigeon: Remove with-database macro, which was a worse version of postmodern:with-connection

--- src/pigeon.lisp
+++ src/pigeon.lisp
@@ -15,19 +15,13 @@
 
 (in-package :ppp)
 
-(defmacro with-database (() &body body)
-  `(unwind-protect
-        (progn
-          (connect-from-url)
-          ,@body)
-     (postmodern:disconnect-toplevel)))
-
 (defun migrate (&optional (direction :up) (target :head) dry-run)
-  (with-database ()
+  (postmodern:with-connection (connect-spec-from-configuration)
     (with-migrations ()
       (execute (solve (current-migration)
                       (find-target-migration direction target))
                :dry-run dry-run))))
+
 
 (defun execute (plan &key dry-run)
   (ppp.migration:ensure-revision-table)
@@ -39,17 +33,19 @@
             (migration-name (car step)))
     (apply #'ppp.migration:migrate `(,@step :dry-run ,dry-run))))
 
-(defun connect-from-url ()
+
+(defun connect-spec-from-configuration ()
   "Takes a URL of form postgres://username:pass@hostname[:port]/database and connects to it."
 
   (multiple-value-bind (proto auth hostname port db) (quri:parse-uri (ppp.configuration:database-url))
     (assert (string-equal proto "postgres"))
     (let ((colon-position (cl:position #\: auth :test #'char=)))
-      (postmodern:connect-toplevel (subseq db 1)
-                                   (subseq auth 0 colon-position)
-                                   (if colon-position (subseq auth (1+ colon-position)) "")
-                                   hostname
-                                   :port (or port 5432)))))
+      (list (subseq db 1)
+            (subseq auth 0 colon-position)
+            (if colon-position (subseq auth (1+ colon-position)) "")
+            hostname
+            :port (or port 5432)))))
+
 
 (defun print-usage (&optional message)
   (format t "~&ppp [command]
@@ -63,17 +59,17 @@ Commands:
     help                         Display this text."
           message))
 
+
 (defun main ()
   (let ((args (uiop:raw-command-line-arguments)))
     (switch ((second args) :test #'string-equal)
       ("describe" (ppp.migration:describe-migrations))
-      ("migrate"  (with-database ()
-                    (migrate (alexandria:make-keyword (string-upcase (third args)))
-                             (let ((target (fourth args)))
-                               (switch (target :test #'string-equal)
-                                 ("head" :head)
-                                 ("base" :base)
-                                 (t (parse-integer target)))))))
+      ("migrate"  (migrate (alexandria:make-keyword (string-upcase (third args)))
+                           (let ((target (fourth args)))
+                             (switch (target :test #'string-equal)
+                               ("head" :head)
+                               ("base" :base)
+                               (t (parse-integer target))))))
       ("new"      (format t "~&Created ~a.~%" (ppp.migration:new-migration (third args))))
       ("help"     (print-usage))
       ("--help"   (print-usage))
